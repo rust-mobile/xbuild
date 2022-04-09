a8da2a4eef2f26a4c58c38b80db4bd36db46779a
David Craven <david@craven.ch>
2022-04-09 20:28:26 +0200

    List versions in x doctor.

--- xcli/src/command/doctor.rs
+++ xcli/src/command/doctor.rs
@@ -1,3 +1,7 @@
+use anyhow::Result;
+use std::path::PathBuf;
+use std::process::Command;
+
 #[derive(Clone, Debug)]
 pub struct Doctor {
     groups: Vec<Group>,
@@ -10,33 +14,52 @@ impl Default for Doctor {
                 Group {
                     name: "clang/llvm toolchain",
                     checks: vec![
-                        "clang",
-                        "clang++",
-                        "llvm-ar",
-                        "llvm-lib",
-                        "lld",
-                        "lld-link",
-                        "lldb",
-                        "lldb-server",
+                        Check::new("clang", Some(VersionCheck::new("--version", 0, 2))),
+                        Check::new("clang++", Some(VersionCheck::new("--version", 0, 2))),
+                        Check::new("llvm-ar", Some(VersionCheck::new("--version", 1, 4))),
+                        Check::new("llvm-lib", None),
+                        Check::new("lld", Some(VersionCheck::new("-flavor ld --version", 0, 1))),
+                        Check::new("lld-link", Some(VersionCheck::new("--version", 0, 1))),
+                        Check::new("lldb", Some(VersionCheck::new("--version", 0, 2))),
+                        Check::new("lldb-server", None), //Some(VersionCheck::new("version", 0, 2))),
                     ],
                 },
                 Group {
                     name: "misc",
-                    checks: vec!["cargo", "git", "flutter"],
+                    checks: vec![
+                        Check::new("cargo", Some(VersionCheck::new("--version", 0, 1))),
+                        Check::new("git", Some(VersionCheck::new("--version", 0, 2))),
+                        Check::new("flutter", Some(VersionCheck::new("--version", 0, 1))),
+                    ],
                 },
                 Group {
                     name: "android",
-                    checks: vec!["adb", "javac", "java"],
+                    checks: vec![
+                        Check::new("adb", Some(VersionCheck::new("--version", 0, 4))),
+                        Check::new("javac", Some(VersionCheck::new("--version", 0, 1))),
+                        Check::new("java", Some(VersionCheck::new("--version", 0, 1))),
+                    ],
                 },
                 Group {
                     name: "ios",
                     checks: vec![
-                        "idevice_id",
-                        "ideviceinfo",
-                        "ideviceinstaller",
-                        "ideviceimagemounter",
-                        "idevicedebug",
+                        Check::new("idevice_id", Some(VersionCheck::new("-v", 0, 1))),
+                        Check::new("ideviceinfo", Some(VersionCheck::new("-v", 0, 1))),
+                        Check::new("ideviceinstaller", Some(VersionCheck::new("-v", 0, 1))),
+                        Check::new("ideviceimagemounter", Some(VersionCheck::new("-v", 0, 1))),
+                        Check::new("idevicedebug", Some(VersionCheck::new("-v", 0, 1))),
                     ],
+                },
+                Group {
+                    name: "linux",
+                    checks: vec![Check::new(
+                        "mksquashfs",
+                        Some(VersionCheck::new("-version", 0, 2)),
+                    )],
+                },
+                Group {
+                    name: "macos",
+                    checks: vec![Check::new("hdiutil", None)],
                 },
             ],
         }
@@ -55,15 +78,21 @@ impl std::fmt::Display for Doctor {
 #[derive(Clone, Debug)]
 struct Group {
     name: &'static str,
-    checks: Vec<&'static str>,
+    checks: Vec<Check>,
 }
 
 impl std::fmt::Display for Group {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         writeln!(f, "{:-^1$}", self.name, 60)?;
         for check in &self.checks {
-            write!(f, "{:20} ", check)?;
-            if let Ok(path) = which::which(check) {
+            write!(f, "{:20} ", check.name())?;
+            if let Ok(path) = check.path() {
+                let version = if let Ok(Some(version)) = check.version() {
+                    version
+                } else {
+                    "unknown".into()
+                };
+                write!(f, "{:20}", version)?;
                 write!(f, "{}", path.display())?;
             } else {
                 write!(f, "not found")?;
@@ -71,6 +100,61 @@ impl std::fmt::Display for Group {
             writeln!(f)?;
         }
         writeln!(f)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Check {
+    name: &'static str,
+    version: Option<VersionCheck>,
+}
+
+impl Check {
+    pub const fn new(name: &'static str, version: Option<VersionCheck>) -> Self {
+        Self { name, version }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct VersionCheck {
+    arg: &'static str,
+    row: u8,
+    col: u8,
+}
+
+impl VersionCheck {
+    pub const fn new(arg: &'static str, row: u8, col: u8) -> Self {
+        Self { arg, row, col }
+    }
+}
+
+impl Check {
+    fn name(self) -> &'static str {
+        self.name
+    }
+
+    fn path(self) -> Result<PathBuf> {
+        Ok(which::which(&self.name)?)
+    }
+
+    fn version(self) -> Result<Option<String>> {
+        if let Some(version) = self.version {
+            let output = Command::new(&self.name)
+                .args(version.arg.split(' '))
+                .output()?;
+            if !output.status.success() {
+                anyhow::bail!("failed to run {}", self.name);
+            }
+            let output = std::str::from_utf8(&output.stdout)?;
+            if let Some(line) = output.split('\n').nth(version.row as _) {
+                if let Some(col) = line.split(' ').nth(version.col as _) {
+                    return Ok(Some(col.to_string()));
+                }
+            }
+            anyhow::bail!("failed to parse version: {:?}", output);
+        } else {
+            Ok(None)
+        }
     }
 }
 
