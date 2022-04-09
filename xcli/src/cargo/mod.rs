bc21498a625cf1505865bddde7e01de897db8064
David Craven <david@craven.ch>
2022-04-09 23:16:20 +0200

    Fix macos/ios builds.

--- xcli/src/cargo/mod.rs
+++ xcli/src/cargo/mod.rs
@@ -209,14 +209,12 @@ impl CargoBuild {
         self.cfg_tool(Tool::Linker, "clang");
         self.use_ld("lld");
         self.set_sysroot(&path);
-        self.add_define("TARGET_OS_OSX", "1");
         self.add_cflag(&format!("-mmacosx-version-min={}", minimum_version));
         self.add_link_arg("--target=x86_64-apple-darwin");
         self.add_link_arg(&format!("-mmacosx-version-min={}", minimum_version));
         self.add_link_arg(&format!("--sysroot={}", path.display()));
         self.add_link_arg("-rpath");
         self.add_link_arg("@executable_path/../Frameworks");
-        self.add_include_dir(&path.join("usr").join("include"));
         self.add_lib_dir(&path.join("usr").join("lib"));
         self.add_lib_dir(&path.join("usr").join("lib").join("system"));
         self.add_framework_dir(&path.join("System").join("Library").join("Frameworks"));
@@ -247,7 +245,6 @@ impl CargoBuild {
         self.add_link_arg(&format!("--sysroot={}", path.display()));
         self.add_link_arg("-rpath");
         self.add_link_arg("@executable_path/Frameworks");
-        self.add_include_dir(&path.join("usr").join("include"));
         self.add_lib_dir(&path.join("usr").join("lib"));
         self.add_framework_dir(&path.join("System").join("Library").join("Frameworks"));
         Ok(())
