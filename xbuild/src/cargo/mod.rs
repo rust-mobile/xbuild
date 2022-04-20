121f241848a169a21a120dcf7f05af38ffcbfeae
Marijn Suijten <marijn@traverseresearch.nl>
2022-04-20 11:26:56 +0200

    xbuild: `--sysroot` link arg is already set by `fn set_sysroot()`

--- xbuild/src/cargo/mod.rs
+++ xbuild/src/cargo/mod.rs
@@ -211,7 +211,6 @@ impl CargoBuild {
         self.add_cflag(&format!("-mmacosx-version-min={}", minimum_version));
         self.add_link_arg("--target=x86_64-apple-darwin");
         self.add_link_arg(&format!("-mmacosx-version-min={}", minimum_version));
-        self.add_link_arg(&format!("--sysroot={}", path.display()));
         self.add_link_arg("-rpath");
         self.add_link_arg("@executable_path/../Frameworks");
         self.add_lib_dir(&path.join("usr").join("lib"));
@@ -241,7 +240,6 @@ impl CargoBuild {
         self.add_cflag(&format!("-miphoneos-version-min={}", minimum_version));
         self.add_link_arg("--target=arm64-apple-ios");
         self.add_link_arg(&format!("-miphoneos-version-min={}", minimum_version));
-        self.add_link_arg(&format!("--sysroot={}", path.display()));
         self.add_link_arg("-rpath");
         self.add_link_arg("@executable_path/Frameworks");
         self.add_lib_dir(&path.join("usr").join("lib"));
