0a23baf8e361a3d5c9c5c2a7e363c66db9f9be35
David Craven <david@craven.ch>
2022-12-09 16:00:46 +0100

    Automate key generation.

--- xbuild/src/command/mod.rs
+++ xbuild/src/command/mod.rs
@@ -2,6 +2,14 @@ use crate::cargo::CrateType;
 use crate::devices::Device;
 use crate::{BuildEnv, CompileTarget, Platform};
 use anyhow::Result;
+use pem::Pem;
+use rand::rngs::OsRng;
+use rsa::pkcs8::{EncodePrivateKey, LineEnding};
+use rsa::RsaPrivateKey;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::Path;
+use x509_certificate::{InMemorySigningKeyPair, Sign, X509CertificateBuilder};
 
 mod build;
 mod doctor;
@@ -58,5 +66,41 @@ pub fn lldb(env: &BuildEnv) -> Result<()> {
     } else {
         anyhow::bail!("no device specified");
     }
+    Ok(())
+}
+
+pub fn generate_key(pem: &Path) -> Result<()> {
+    RsaPrivateKey::new(&mut OsRng, 2048)?.write_pkcs8_pem_file(pem, LineEnding::CRLF)?;
+    Ok(())
+}
+
+pub fn generate_csr(pem: &Path, csr: &Path) -> Result<()> {
+    let pem = std::fs::read_to_string(pem)?;
+    let pem = pem::parse_many(pem)?;
+    let key = if let Some(key) = pem.iter().find(|pem| pem.tag == "PRIVATE KEY") {
+        InMemorySigningKeyPair::from_pkcs8_der(&key.contents)?
+    } else {
+        anyhow::bail!("no private key found");
+    };
+    let mut builder = X509CertificateBuilder::new(key.key_algorithm().unwrap());
+    builder
+        .subject()
+        .append_common_name_utf8_string("Apple Code Signing CSR")
+        .expect("only valid chars");
+    let pem = builder
+        .create_certificate_signing_request(&key)?
+        .encode_pem()?;
+    std::fs::write(csr, pem)?;
+    Ok(())
+}
+
+pub fn add_certificate(pem: &Path, cer: &Path) -> Result<()> {
+    let mut f = OpenOptions::new().write(true).append(true).open(pem)?;
+    let cer = std::fs::read(cer)?;
+    let pem = pem::encode(&Pem {
+        tag: "CERTIFICATE".into(),
+        contents: cer,
+    });
+    f.write_all(pem.as_bytes())?;
     Ok(())
 }
